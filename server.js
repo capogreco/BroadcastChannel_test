3d4845be730a47d2e6402e181a3527a09b0ec1a9
capo_greco <thomas.capogreco@gmail.com>
2024-01-15 20:22:43 +1100

    binding server to req_handler for serve

--- server.js
+++ server.js
@@ -2,4 +2,4 @@ import { serve      } from "https://deno.land/std@0.185.0/http/server.ts"
 import { ServerNode } from "./modules/ServerNode.js"
 
 const server = new ServerNode ()
-serve (server.req_handler, { port: 80 })
+serve (server.req_handler.bind (server), { port: 80 })
